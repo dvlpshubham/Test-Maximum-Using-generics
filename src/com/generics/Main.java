7cbc56e8592c313016e18d8124def4b985a51ee0
Shubham Raj <shubhamaryan805@gmail.com>
2022-11-22 21:03:34 +0530

    Added : Maximum of Three Integers

---
+++ src/com/generics/Main.java
@@ -0,0 +1,14 @@
+package com.generics;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to check max of three variables");
+
+        Integer a = 19, b = 24, c = 25;
+
+        com.generics.TestMaximum.testMaximum(a, b, c);
+
+    }
+
+}
