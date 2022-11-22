c0c0f14f6c067839de77a264e269ff7d0d5650f0
Shubham Raj <shubhamaryan805@gmail.com>
2022-11-22 21:23:59 +0530

    Refactored: to generic to find maximum

--- src/com/generics/Main.java
+++ src/com/generics/Main.java
@@ -1,9 +1,14 @@
 package com.generics;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Check maximum of three variables");
-        String x = "apple", y = "peach", z = "banana";
+        System.out.println("Welcome to check max of three variables");
+        String x = "jack", y = "king", z = "queen";
+        Integer a = 25, b = 24, c = 19;
+        Float p = 1.23f, q = 2.2f, r = 0.2f;
         TestMaximum.testMaximum(x, y, z);
+        TestMaximum.testMaximum(a, b, c);
+        TestMaximum.testMaximum(p, q, r);
     }
 }
--- src/com/generics/TestMaximum.java
+++ src/com/generics/TestMaximum.java
@@ -1,29 +1,26 @@
 package com.generics;
 
-public class TestMaximum {
-    String x, y, z;
-
-    // constructor
-    public TestMaximum(String x, String y, String z) {
+public class TestMaximum <T extends Comparable<T>>{
+    T x,y,z;
+    //constructor
+    public TestMaximum(T x, T y, T z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
+    //checking max of three integers
+    public static<T extends Comparable<T>> T testMaximum(T x,T y,T z){
+        T max=x;//initialising x as greater
+        if(y.compareTo(max)>0)
+            max=y;//as of now y is greater
 
-    // checking max of three integers
-    public static String testMaximum(String x, String y, String z) {
-        String max = x;// initialising x as greater
-        if (y.compareTo(max) > 0)
-            max = y;// as of now y is greater
-
-        if (z.compareTo(max) > 0)
-            max = z;// now z is greater
-        printMax(x, y, z, max);
+        if(z.compareTo(max)>0)
+            max=z;//now z is greater
+        printMax(x,y,z,max);
         return max;
 
     }
-
-    public static void printMax(String x, String y, String z, String max) {
-        System.out.printf("The maximum from string of %s , %s and %s is %s\n", x, y, z, max);
+    public static <T>void printMax(T x,T y,T z,T max){
+        System.out.printf("the max of %s , %s and %s is %s\n",x,y,z,max);
     }
 }
